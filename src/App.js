9a8166f44b59ee0c62602f8b5b8469eed8f9e00d
unknown <sidduinevitable@gmail.com>
2022-08-19 17:08:51 +0530

    create PeopleList component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import { AddPeople } from "./components/AddPeople";
-import { usePeopleStore } from "./store/peoplestore";
+import { PeopleList } from "./components/PeopleList";
 
 function App() {
-  const people = usePeopleStore((state) => state.people);
   return (
     <>
       <h1>People list</h1>
       <AddPeople />
-      <ul>
-        {people.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
+      <PeopleList />
     </>
   );
 }
