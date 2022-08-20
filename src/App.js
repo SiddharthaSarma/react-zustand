06a9f1627ce878a8a61b640d1aa405f00882714b
unknown <sidduinevitable@gmail.com>
2022-08-20 21:44:57 +0530

    add theme store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
 import { AddPeople } from "./components/AddPeople";
 import { PeopleList } from "./components/PeopleList";
+import { useThemeStore } from "./store/themestore";
 
 function App() {
+  const dark = useThemeStore((state) => state.dark);
+  const setTheme = useThemeStore(state => state.toggleDarkMode);
+  useEffect(() => {
+    if (dark) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [dark]);
   return (
     <>
+      <button onClick={setTheme}>Toggle theme</button>
       <h1>People list</h1>
       <AddPeople />
       <PeopleList />
